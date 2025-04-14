d99450b6f203acc26e3541a958d8401052f71720
Daniil Palii <daniil.palii@proton.me>
2025-04-14 16:45:23 +0200

    Refactor
    - move component code to separate files
    - remove unnecessary comments
    - remove empty lines

---
+++ BlazorCv/Components/Pages/Error/Error.razor.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorCv.Components.Pages.Error;
+
+public partial class Error
+{
+	[CascadingParameter] private HttpContext? HttpContext { get; set; }
+	private string? RequestId { get; set; }
+	private bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+	protected override void OnInitialized() =>
+		RequestId = Activity.Current?.Id ?? HttpContext?.TraceIdentifier;
+}
